4cd831c50d531fe4a729e735a7fd1b423abf4843
Zilargron <christiantupay20@gmail.com>
2025-07-07 07:43:40 +0800

    Remove validlogin.ts

--- e2e-projects/saucedemo/utils/validlogin.ts
+++
@@ -1,9 +0,0 @@
-import {Page} from 'playwright';
-
-export async function validlogin(page : Page){
-
-    await page.goto('https://www.saucedemo.com');
-    await page.fill('#user-name', 'standard_user');
-    await page.fill('#password','secret_sauce');
-    await page.getByRole('button', {name : 'Login'}).click();
-}
