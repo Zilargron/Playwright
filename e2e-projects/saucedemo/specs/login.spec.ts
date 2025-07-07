475b2adf9e16b2cadf4e4235f7c72798145d2bbb
Zilargron <christiantupay20@gmail.com>
2025-07-07 09:38:19 +0800

    Add logout spec for valid users only.

--- e2e-projects/saucedemo/specs/login.spec.ts
+++ e2e-projects/saucedemo/specs/login.spec.ts
@@ -1,15 +1,17 @@
-import {test, expect} from '@playwright/test';
-import {loginwithcredentials} from '../utils/loginwithcredentials.ts';
-import {users} from '../test-data/users.ts'
+import { test, expect } from '@playwright/test';
+import { loginwithcredentials } from '../utils/loginwithcredentials';
+import { users } from '../test-data/users';
 
-for (const user of users){
-    test(`Login test for ${user.username}`, async ({page}) => {
-        await loginwithcredentials(page, user.username, user.password);
+test.describe('Login tests for all users', () => {
+  for (const user of users) {
+    test(`Login test for ${user.username}`, async ({ page }) => {
+      await loginwithcredentials(page, user.username, user.password);
 
-        if (user.valid){
-            await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-        } else {
-            await expect(page.locator('[data-test="error"]')).toBeVisible();
-        }
+      if (user.valid) {
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+      } else {
+        await expect(page.locator('[data-test="error"]')).toBeVisible();
+      }
     });
-}
+  }
+});
