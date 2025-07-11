77ee8a6872caca85fb2f63941aefcecadc2fa0f9
Zilargron <christiantupay20@gmail.com>
2025-07-11 08:32:05 +0800

    WIP: saving unstaged changes before rebase.

--- Practice/saucedemo-practice/test-data/users.ts
+++
@@ -1,8 +0,0 @@
-export const users = [
-    {username:'standard_user', password:'secret_sauce', valid:true},
-    {username:'locked_out_user', password:'secret_sauce', valid:false},
-    {username:'problem_user', password:'secret_sauce', valid:true},
-    {username:'performance_glitch_user', password:'secret_sauce', valid:true},
-    {username:'error_user', password:'secret_sauce', valid:true},
-    {username:'visual_user', password:'secret_sauce', valid:true}
-   ];
--- Practice/saucedemo-practice/utils/loginwithcredentials.ts
+++
@@ -1,9 +0,0 @@
-    import { Page } from '@playwright/test';
-
-    export async function loginwithcredentials(page:Page, username:string, password:string) {
-
-        await page.goto('https://www.saucedemo.com');
-        await page.fill('#user-name', username);
-        await page.fill('#password', password);
-        await page.getByRole('button', {name:'Login'}).click();
-    }
